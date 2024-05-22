cba1937a5f859ed397d3f83cf79ba3a2e72254f6
LucasR-Souza <lucasre.350@gmail.com>
2024-05-22 13:15:52 -0300

    Update main.js
    Código concertado

--- Trabalho Streaming/main.js
+++ Trabalho Streaming/main.js
@@ -1,15 +1,5 @@
-//carrossel de imagens
-
-const imagem1 = document.getElementById('imagem_1');
-const imagem2 = document.getElementById(imagem_2)
-const imagem3 = document.getElementById(imagem_3)
-
-imagem1.src = "./Imagem/background1.png"
-console.log
-
-
 //Redirecinamento dos botoes dos planos
-const botoesPlano = document.getElementsByClassName('plano_botao');
+const botoesPlano = document.getElementsByClassName('redirecionar_registro');
 
 for(let i = 0; i < botoesPlano.length; i++){
     botoesPlano[i].addEventListener('click', function(){
