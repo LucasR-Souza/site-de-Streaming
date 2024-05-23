7d27bd58d5a80f9baa36a923b3ae51d526031db3
LucasR-Souza <lucasre.350@gmail.com>
2024-05-23 10:56:34 -0300

    Atualização na recursividade da página

--- Trabalho Streaming/main.js
+++ Trabalho Streaming/main.js
@@ -6,3 +6,28 @@ for(let i = 0; i < botoesPlano.length; i++){
         window.location.href = "./src/registro.html"
     });
 };
+
+//Esconder e exibir elementos do nav
+
+function VerificarTamanhoDaTela(){
+    if(window.matchMedia('(max-width: 780px)').matches){
+        esconderNav();
+    }else{
+        mostrarNav();
+    }
+}
+
+function esconderNav(){
+    document.getElementsByClassName('nav_alterar')[0].classList.add('hidden')
+    document.getElementsByClassName('nav_alterar')[1].classList.add('hidden')
+    document.getElementsByClassName('nav_registro')[0].classList.remove('hidden')
+}
+function mostrarNav(){
+    document.getElementsByClassName('nav_alterar')[0].classList.remove('hidden')
+    document.getElementsByClassName('nav_alterar')[1].classList.remove('hidden')
+    document.getElementsByClassName('nav_registro')[0].classList.add('hidden')
+}
+
+VerificarTamanhoDaTela();
+
+window.addEventListener('resize', VerificarTamanhoDaTela);
